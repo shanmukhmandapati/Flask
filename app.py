00f64aea9d9afe10fc671cd80815665b453e49d9
Shanmukh Mandapati <shanmukh.m.rao@gmail.com>
2023-07-26 23:20:32 +0530

    Flask Commit

---
+++ app.py
@@ -0,0 +1,63 @@
+## create a simple flask application
+
+from flask import Flask,render_template, request, redirect, url_for
+
+## create flask app
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h2>Hello World !</h2>"
+
+@app.route('/welcome')
+def welcome():
+    return "Welcome to the flask class!"
+
+@app.route('/login')
+def login():
+    return "Please login to session"
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/success/<int:score>')
+def success(score):
+    return "the person is passed "+str(score)
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "the person has failed "+str(score)
+
+@app.route('/calculate', methods = ['POST', 'GET'])
+def calculate():
+    if request.method =='GET':
+        return render_template('calculate.html')
+    else:
+        maths = float(request.form['maths'])
+        science = float(request.form['science'])
+        history = float(request.form['history'])
+
+        average_marks = (maths+science+history)/3
+
+        result= ''
+
+        if average_marks > 50:
+            result = 'success'
+        else:
+            result = 'fail'
+
+       # return redirect(url_for(result,score = average_marks))
+
+        return render_template('result.html', results = average_marks)
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
